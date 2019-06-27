e392cb178ce872d592c7d3342f1a2ff5a373428c
Ayushi-12 <asthana.ayushi12@gmail.com>
2019-06-27 19:26:04 +0530

    removing useless and redundant scripts

--- findSP500Deciles.py
+++
@@ -1,9 +0,0 @@
-import pandas
-import numpy
-import matplotlib
-
-df = pandas.read_csv('sp500marketcap.csv')
-
-tsymList = sorted(list(set(df['TICKER'])))
-print tsymList
-print len(tsymList)
--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-
-df = pd.read_csv('graph.csv')
-print(df)
